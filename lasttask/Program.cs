23ff985ccacf39f785d8d8b0aece5a1e50c344f6
Vladislav <vlad-taranin@yandex.ru>
2023-01-08 15:21:28 +0300

    complete zadanie

---
+++ lasttask/Program.cs
@@ -0,0 +1,27 @@
+﻿string[] array1 = new string[5] {"Russian", "12", "Education", "hi", "program" };
+string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+System.Console.WriteLine("Вывод оригинального массива");
+PrintArray(array1);
+System.Console.WriteLine("Вывод нового массива");
+PrintArray(array2);
